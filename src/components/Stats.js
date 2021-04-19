23cf27124bdbc300ad65612442253295d6454a57
jwatts82 <wattscodes@gmail.com>
2021-04-19 14:20:24 -0700

    access player stats data

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,22 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 const Stats = () => {
+    const { playerId, fullName, playerStats, playerImage, gameDate, team, teamIMage, opponent, opponentImage, PA, AB, H, HR, BB, K, HBP, SF, TB, RBI } = useContext(PlayerContext)
     const { id } = useParams()
      
         return (
             <div>
                 <h1>I am A Player Page - { id }</h1>
+                <p>{JSON.stringify(playerStats)}</p>
             </div>
         )
     
 }
 
 export default Stats
-
-
-// <PlayerContext.Consumer>{(context) => {
-//     return (
-//         JSX
-//     )
-// }}</PlayerContext.Consumer>
